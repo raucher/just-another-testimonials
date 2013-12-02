03a6d22ad90e14e232c7fd96e17d06db154cd9b2
raucher <myplace4spam@gmail.com>
2013-12-02 21:33:15 +0200

    table added to main_menu

--- templates/main_menu.php
+++ templates/main_menu.php
@@ -10,4 +10,40 @@
   */
 ?>
 
-<h1>Voila, main menu page!!</h1>
+<div class="wrap">
+	<h2>Manage Testimonials</h2>
+
+	<form method="post" action="options.php">
+		<?php settings_fields('jtstm_testimonials_group') ?>
+		<?php $tstm = get_option('jststm_testimonials') ?>
+		<table class="widefat" style="width: 80%">
+			<thead>
+				<tr>
+					<th>Message</th>
+					<th>Author</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr class="item-conatiner">
+					<td style="width: 60%">
+						<textarea name="" style="width: 90%" rows="7"></textarea>
+					</td>
+					<td style="width: 50%">
+						<input type="text" style="width: 90%">
+						<a href="#" style="display: block;">Delete Item</a>
+						<a href="#" style="display: block;">Add new</a>
+					</td>
+				</tr>
+			</tbody>
+
+			<tfoot>
+				<tr>
+					<th>Message</th>
+					<th>Author</th>
+				</tr>
+			</tfoot>
+		</table>
+	</form>
+
+</div>
