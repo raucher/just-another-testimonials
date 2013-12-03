c315151205340edf8391464b4f951e8e1560694e
raucher <myplace4spam@gmail.com>
2013-12-03 16:52:21 +0200

    front-end template created

--- templates/front_end.php
+++ templates/front_end.php
@@ -2,6 +2,19 @@
  /**
   * Front-end template
   *
+  * Plugin option name: jststm_testimonials
+  *
   * @package just-another-testimonials
   * @author raucher <myplace4spam@gmail.com>
   */
+?>
+
+<div class="jststm-container">
+	<h3>Hi, I'm a shortcode o_0 !</h3>
+	<?php foreach (get_option('jststm_testimonials') as $tstm):?>
+		<blockquote>
+			<p class="message"><?php echo $tstm['message'] ?></p>
+			<p class="author"><small><?php echo $tstm['author'] ?></small></p>
+		</blockquote>
+	<?php endforeach ?>
+</div>
