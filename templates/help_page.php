4d9d934fb4aede8d5e595595cd43584ee1505e43
raucher <myplace4spam@gmail.com>
2013-12-03 21:43:11 +0200

    help page done

--- templates/help_page.php
+++ templates/help_page.php
@@ -7,4 +7,14 @@
   */
 ?>
 
-<h1>Help page</h1>
+<div class="wrap" style="font-size: 16px;">
+	<h2><?php _e('What is this and what the plugin does?', 'jststm-domain') ?></h2>
+	<hr>
+
+	<p style="width: 70%;">
+		<?php _e('This plugin allows you to create testimonials list and neatly show them to your visitors', 'jststm-domain') ?>
+	</p>
+
+	<h3 style="font-weight: normal; color: #464646;"><?php _e('Example shortcode:', 'jststm-domain') ?></h3>
+	<code style="font-size: 16px;">[jst-testimonials]</code>
+</div>
